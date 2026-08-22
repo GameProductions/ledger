5b7e2cef3f79d415b540741689f15e1465e829cb
Vic <morse@gameproductions.com>
2026-08-22 00:46:55 -0400

    [v3.160.4] feat(footer): update footer layout to match fleet standard

--- apps/web/components/layout/GlobalLayout.tsx
+++ apps/web/components/layout/GlobalLayout.tsx
@@ -7,11 +7,8 @@ export const GlobalLayout: React.FC<{ children: React.ReactNode }> = ({ children
       <main className="flex-grow flex flex-col w-full">
         {children}
       </main>
-      <div className="w-full flex justify-center">
-        <div className="w-full max-w-7xl">
-          <GlassFooter />
-        </div>
-      </div>
+      <GlassFooter />
     </div>
   );
 };
+
